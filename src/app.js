d43c231366d5c839bf7c778b2180ee3c0ea77736
andressafirmino <andressafirmino78@gmail.com>
2023-06-23 21:15:36 -0300

    Versão com POST/sign-up

--- src/app.js
+++ src/app.js
@@ -1,6 +1,28 @@
-import { express } from "express";
+import express from "express";
+import cors from "cors";
 
 const app = express();
-app.listen(5000);
 
-console.log('foi')
+app.use(cors());
+app.use(express.json());
+
+const nickname = [];
+
+app.post("/sign-up", (req, res) => {
+    nickname.push({
+        username: req.body.username, 
+        avatar: req.body.avatar
+    })
+    res.send('OK');
+   
+})
+
+
+
+app.get("/tweets", (request, response) => {
+    response.send('foi');
+})
+
+const PORT = 5000;
+app.listen(PORT, () => console.log(`O servidor está rodando na porta ${PORT}...`));
+
